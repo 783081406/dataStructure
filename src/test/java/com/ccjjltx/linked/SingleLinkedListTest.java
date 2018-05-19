d9905141689ca43d88c34b39c21f56c09d24e1e2
783081406 <783081406ccj@gmail.com>
2018-05-19 18:11:04 +0800

    单链表

---
+++ src/test/java/com/ccjjltx/linked/SingleLinkedListTest.java
@@ -0,0 +1,21 @@
+package com.ccjjltx.linked;
+
+import org.junit.Test;
+
+public class SingleLinkedListTest {
+    @Test
+    public void testSingleLinkedList() {
+        SingleLinkedList singleList = new SingleLinkedList();
+        singleList.addHead("A");
+        singleList.addHead("B");
+        singleList.addHead("C");
+        singleList.addHead("D");
+        //打印当前链表信息
+        singleList.display();
+        //删除C
+        singleList.delete("C");
+        singleList.display();
+        //查找B
+        System.out.println(singleList.find("B"));
+    }
+}
