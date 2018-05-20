508d89d527d66327c7bb250987523e3275dd176f
783081406 <783081406ccj@gmail.com>
2018-05-20 14:07:00 +0800

    汉诺塔

---
+++ src/test/java/com/ccjjltx/recursion/HanoiTowerTest.java
@@ -0,0 +1,10 @@
+package com.ccjjltx.recursion;
+
+import org.junit.Test;
+
+public class HanoiTowerTest {
+    @Test
+    public void test1() {
+        HanoiTower.move(3, "A", "B", "C");
+    }
+}
